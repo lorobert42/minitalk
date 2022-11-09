9cd737c1a9ae805e3ab1e7e1ab58c6c9994ca7b2
lorobert <lorobert@student.42lausanne.ch>
2022-11-09 14:58:22 +0100

    Base

--- minitalk_client.c
+++ minitalk_client.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_client.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lorobert <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 14:52:56 by lorobert          #+#    #+#             */
+/*   Updated: 2022/11/09 14:52:58 by lorobert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main() {
-	printf("Hello, World!\n");
+#include <signal.h>
+#include "libft/libft.h"
+
+int main(int argc, char **argv)
+{
+	int	pid;
+
+	(void)argc;
+	pid = ft_atoi(argv[1]);
+	kill(pid, SIGUSR1);
 	return 0;
 }
--- minitalk_server.c
+++ minitalk_server.c
@@ -1,6 +1,29 @@
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_server.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lorobert <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 14:47:11 by lorobert          #+#    #+#             */
+/*   Updated: 2022/11/09 14:47:15 by lorobert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main() {
-	printf("Hello, World!\n");
-	return 0;
+#include <signal.h>
+#include <unistd.h>
+#include "libft/libft.h"
+
+void	sig_handler(int signum)
+{
+	(void)signum;
+	ft_printf("Received signal\n");
+}
+
+int	main()
+{
+	ft_printf("%d\n", getpid());
+	signal(SIGUSR1, sig_handler);
+	pause();
+	return (0);
 }
